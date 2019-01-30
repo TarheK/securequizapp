48cc80e4d03be45c826ae34f3cd6c34ccfb10112
ConnellyR <42652898+ConnellyR@users.noreply.github.com>
2019-01-29 16:26:54 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -74,18 +74,27 @@ def renderPage6():
     #TODO: set the favorite color in the session
      session["fifth"] = request.form["fifth"] #adds the favorite color to the cookie
      score=0; 
-    if session["first"]=2:
+     if session["first"]=2:
       score=score+1
-    else:
+     else:
       score=score+0
-    if session["second"]=63:
+     if session["second"]=63:
       score=score+1
-    else:
+     else:
       score=score+0
-    if session["third"]=5:
+     if session["third"]=125:
       score=score+1
-    else:
+     else:
       score=score+0
+     if session["forth"]=5:
+      score=score+1
+     else:
+      score=score+0
+     if session["fifth"]=c:
+      score=score+1
+     else:
+      score=score+0
+     print(score);
      return render_template('page6.html')
 
 if __name__=="__main__":
